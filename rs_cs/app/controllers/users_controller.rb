3c5b5e7ae897c1059209547900bf10a7503a5834
Logan Hartman <loganchart@gmail.com>
2022-12-14 16:44:32 -0500

    userS fixed

--- rs_cs/app/controllers/users_controller.rb
+++ rs_cs/app/controllers/users_controller.rb
@@ -1,15 +1,24 @@
 class UsersController < ApplicationController
 
    def index
-      render plain:"I'm in the index action!"
-   end
+        debugger
+        @users = User.all
+        render json: @users
+   end 
 
    def create
-      render json: params
+        @user = User.new(user_params)
+        @user.save!
+        render json: user
    end
 
    def show
       render json: params
+   end
+
+   private
+   def user_params
+    params.require(:user).permit(:name, :email)
    end
 
 end
