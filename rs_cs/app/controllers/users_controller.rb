5d46a19d143e42f029b0c409c13d8c8081bceda7
Logan Hartman <loganchart@gmail.com>
2022-12-15 16:53:34 -0500

    question for Kin

--- rs_cs/app/controllers/users_controller.rb
+++ rs_cs/app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
    end 
 
    def create
+      
         user = User.new(user_params)
         if user.save
             render json: user
@@ -15,15 +16,20 @@ class UsersController < ApplicationController
    end
 
    def show
+      
       @user = User.find(params[:id])
       render json: @user
    end
 
    def update
+      
       @user = User.find(params[:id])
       if @user.update(user_params)
-        redirect_to user_url(@user)
+         
+         render json: @user
+      #   redirect_to user_url(@user)
       else
+         
         render json: @user.errors.full_messages, status: 422
       end
    end
@@ -36,6 +42,7 @@ class UsersController < ApplicationController
 
    private
    def user_params
+      
       params.require(:user).permit(:username)
    end
 
