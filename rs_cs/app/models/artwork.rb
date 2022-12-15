dfd50a245ce997ff27983a2f11ceebad1e9b1e99
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2022-12-15 11:17:41 -0500

    Creation of validations and foreign keys between Users (artists) and Artwork.

--- rs_cs/app/models/artwork.rb
+++ rs_cs/app/models/artwork.rb
@@ -1,6 +1,11 @@
 class Artwork < ApplicationRecord
     validates :image_url, presence: true, uniqueness: true
-    validates :artists_id, presence: true
+    validates :artist_id, presence: true
     
+    validates :title, uniqueness: { scope: :artist_id, message: "Artist has already created piece"}
 
+    belongs_to :artist,
+        primary_key: :id,
+        foreign_key: :artist_id,
+        class_name: :User
 end
--- rs_cs/db/seeds.rb
+++ rs_cs/db/seeds.rb
@@ -6,5 +6,17 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user.destroy.all
+User.destroy_all
+Artwork.destroy_all
+
+arty = User.create!(username: "artyartytheonemanyparty")
+tobie = User.create!(username: "Toobtobbletobe")
+vangogh = User.create!(username: "dontbitemeear")
+
+art1 = Artwork.create!(title: "dog with bone", image_url: "12345", artist_id: vangogh.id)
+art2 = Artwork.create!(title: "man on boat with bone", image_url: "12344", artist_id: tobie.id)
+art3 = Artwork.create!(title: "white canvas 1", image_url: "12322", artist_id: arty.id)
+art4 = Artwork.create!(title: "white canvas 2", image_url: "12321", artist_id: arty.id)
+art5 = Artwork.create!(title: "sad boy", image_url: "12111", artist_id: tobie.id)
+art6 = Artwork.create!(title: "Girl with a pearl earring", image_url: "10000", artist_id: tobie.id)
 
